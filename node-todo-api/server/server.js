730e42b0cbae7fb64b2f11d07c3ba458e86cc72a
XKalden <kaldend.tsering@outlook.com>
2018-08-17 12:11:33 -0400

    added query for route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,6 +6,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 
+const {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -16,16 +18,47 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    let wow = req.body.text;
-
-    console.log(typeof wow); 
-
     todos.save().then(doc => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
     });
 });
+
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, e => res.status(400).send(e));
+
+});
+
+
+// Get /todos/12341
+app.get('/todos/:id', (req, res) => {
+
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((tood) => {
+        if(!tood){
+            return res.status(404).send('Wrong ID');
+         
+        }
+        res.status(200).send({tood});
+    })
+    .catch((e)=> res.status(400).send(e));
+
+
+});
+
+
+
+
+
+
 
 
 app.listen(3000, () => {
